5d6360ede23ec17656fe5d9ac03a8dc0ed7afe74
Rémi <Remi Pepin>
2019-02-02 12:36:35 +0100

    polar chart ice

---
+++ ice_melt_polar_chart.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+
+#Import des données
+ice_melt<- read.table("data/ice_melt.txt", quote="\"", comment.char="")
+
+#Renommage des colonnes
+colnames(ice_melt) <- c("année",  "janvier", "fevrier", "mars", "avril", "juin","mai", "juillet", "août", "septembre", "octobre", "novembre", "décembre")
+
+#Besoin de modifier les données pour faire le graph. On va faire une table avec comme colonne, mois, année, valeur de glace
+ice_melt_clean<- data.frame("mois" = rep(c("Jan", "Feb", "Mar", "Apr", "May","Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),39)
+                , "value"=0
+                , "année" = 0)
+
+#Besoin de l'ordre des mois sinon le graph ne resemble à rien
+ice_melt_clean$mois = factor(ice_melt_clean$mois, levels = month.abb)
+
+#Bon je ne suis pas un expert de R, et il y a peut-être plus R-friendly.  Le principe, on va lire les données d'une ligne (= une année)
+#pour les mettre dans un vecteur qu'on va ensuite coller dans le colonne value. Et au passage on génère aussi la colone année
+valeursFinales <-c()
+année <-c()
+
+for (i in 1:39) {
+  valeursFinales <- c(valeursFinales,as.numeric(ice_melt2 [i,]))
+  année <- c(année, rep(1978+i, 12))
+}
+
+#On colle tout ça dans les colones
+ice_melt_clean[,2] <- valeursFinales
+ice_melt_clean[,3] <-année
+
+# Tel quel le diagrame va avoir un trou entre décembre et janvier, alors on va ruser et faire des 'ponts'
+bridges <- ice_melt_clean[ice_melt_clean$mois == 'Jan',] #récupére donnée de janvier
+bridges$année <- bridges$année - 1    # On décale pour mettre ça à l'année n-1
+bridges$mois <- NA    # On met la valeur de mois à NA pour ensuite la virer
+
+
+ggplot(rbind(x, bridges), aes(mois, value,group=année, color=année)) + 
+  geom_line() + # on fait un diagramme basique en ligne
+  scale_x_discrete(expand = c(0,0), breaks = month.abb, name=NULL) + # On ferme les trous et on redéfini les labels pour ne pas avoir NA
+  scale_y_continuous(name="Volume de glace (10³km³)") + 
+  coord_polar(theta = "x",start=-pi*1/12) + # On fait un diagramme en coordonnées polaire et la magie s'opère <3
+  scale_color_continuous(name=NULL) + 
+  theme_minimal()+
+  labs(
+    title    = "La spirale infernale de la fonte des glaces",
+    subtitle = "PIOMAS Arctic Sea Ice Volume (10³km³)", 
+    caption = "Source: http://psc.apl.uw.edu/research/projects/arctic-sea-ice-volume-anomaly/data/"
+  )
+ 
