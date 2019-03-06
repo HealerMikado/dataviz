c57c1c7f2bf4301568e003b3da46299a683207b6
Rémi Pépin <remi.pepin@insee.fr>
2019-03-06 15:58:59 +0100

    svg amélioré

--- tp5/svg amélioré/js.js
+++ tp5/svg amélioré/js.js
@@ -1,97 +1,104 @@
-// récupérage des éléments du DOM
-var graphic = document.getElementById("graphic");
-var buttons = document.getElementsByTagName("button");
-var reinitialize = document.getElementById("reinitialize");
-var sidebar = document.getElementById("sidebar");
-// var random       = document.getElementById("random");
-
-// création d'un nouvel élément du DOM
-var warning = document.createElement("p");
-warning.id = 'warning';
-warning.textContent = "Chargement en cours";
-
-// création d'un nouvel élément du DOM
-var graph =  document.createElement("svg");
-document.getElementById("content").appendChild(graph);  
-
-// définitions des variables
-var url_initial = graphic.src;
-var url_current = graphic.src;
-var urls_for_random_graphic = [
-  "http://www.vackerunderbar.se/wp-content/uploads/2010/08/gapminder.png",
-  "https://cdn.quotesgram.com/img/16/29/360148697-DSC8242hans-rosling.jpg",
-  "https://i0.wp.com/econlife.com/wp-content/uploads/2017/02/Gapminder_World-1.jpg",
-  "http://www.stashmedia.tv/wp-content/uploads/2010/12/rosling.png"
+// Données
+var countries = [
+  {
+    x: 102,
+    y: 345,
+    text: "Pays 1",
+    color: "#E89777"
+  },
+  {
+    x: 34,
+    y: 67,
+    text:  "Pays 2",
+    color: "#E89777"
+  },
+  {
+    x: 563,
+    y: 241,
+    text:  "Pays 3",
+    color: "#E89777"
+  },
+  {
+    x: 120,
+    y:  52,
+    text:  "Pays 4",
+    color: "#98A7F0"
+  },
+  {
+    x: 467,
+    y: 207,
+    text:  "Pays 5",
+    color: "#98A7F0"
+  },
+  {
+    x: 366,
+    y: 291,
+    text: "Pays 6",
+    color: "#98A7F0"
+  },
+  {
+    x: 111,
+    y: 34,
+    text: "Pays 7",
+    color: "#98A7F0"
+  },
+  {
+    x: 234,
+    y: 345,
+    text: "Pays 8",
+    color: "#54BC9A"
+  },
+  {
+    x: 509,
+    y: 344,
+    text: "Pays 9",
+    color: "#54BC9A"
+  },
+  {
+    x: 375,
+    y: 120,
+    text: "Pays 10",
+    color: "#54BC9A"
+  }
 ];
 
-// définition des fonctions
-function load_image(event) {
+// Sélection/création des éléments du DOM
 
-  var button = event.target;
+/* Question 6 */
 
-  if (button.id == "reinitialize") {
+let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+document.getElementById("content").appendChild(svg);
+// document.getElementById("content").insertBefore(svg, null);
 
-    url_current = url_initial;
+/* Question 7 */
 
-  } else if (button.id == "random") {
+for(let i=0;i<countries.length;i++){
+  
+  let group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  let text   = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  circle.setAttribute('cx',     countries[i].x);
+  circle.setAttribute('cy',     countries[i].y);
+  circle.setAttribute('fill',   countries[i].color);
+  circle.setAttribute('stroke', countries[i].color);
+  text.textContent =            countries[i].text;
+  text.setAttribute('x',        countries[i].x+10);
+  text.setAttribute('y',        countries[i].y+5);
+  text.setAttribute('fill',     countries[i].color);
+  svg.appendChild(group);
+  group.appendChild(circle);
+  group.appendChild(text);
+} 
 
-    var urls = urls_for_random_graphic.filter(
-      url => url != url_current
-    );
-    url_current = urls[Math.floor(Math.random() * urls.length)];
-  }
-
-  /* charger une image peut prendre du temps
-  nous simulons ça par une attente de 2s
-  avant de générer l'événement "image_ready" ;
-  pendant ce temps, le reste du code continue à être exécuté */
-  graphic.src = url_current;
-  setTimeout(function() {
-    var image_ready = new Event('image_ready');
-    /* Le fait que l'image soit prête à être affichée
-    ne concerne pas le bouton lui même! L'événement 
-    est domc généré directement au niveau
-    du document tout entier. */
-    document.dispatchEvent(image_ready);
-  }, 2000); // 2000 ms = 2s
+// Actions
+function toggle_circle() {
+  this.classList.toggle("selected");
 }
 
-var add_warning = function() {
-  sidebar.insertBefore(warning, null);
-  // Le javascript standard requiert à la fois un élément parent ET un élément frère pour insérer un nouvel élément. (Le nouvel élément est inséré juste devant le frère. "null" signifie donc en dernière position!) De nombreuses bibliothèques simplifient cette syntaxe alambiquée.
+// Abonnements
+
+/* Question 8 */
+let circles  = document.getElementsByTagName("circle");
+for (let circle of circles) {
+    circle.addEventListener('click', toggle_circle)
 }
-
-function remove_warning() {
-  sidebar.removeChild(warning);
-}
-
-var blur_image = function() {
-  graphic.style.opacity = 0.5;
-}
-
-var unblur_image = function() {
-  graphic.style.opacity = 1;
-}
-
-/*
-    correctoin question 3
-*/
-function toggleCircle(){
-    var cercles = document.getElementsByTagName("circle");
-    Array.from(cercles).forEach(cercle => {
-        cercle.classList.toggle("selected")
-    });
-}
-firstCircle.addEventListener("click", toggleCircle);
-
-
-// initialisation
-reinitialize.addEventListener("click", load_image);
-reinitialize.addEventListener("click", blur_image);
-reinitialize.addEventListener("click", add_warning);
-random.addEventListener("click", load_image);
-random.addEventListener("click", blur_image);
-random.addEventListener("click", add_warning);
-document.addEventListener("image_ready", unblur_image);
-document.addEventListener("image_ready", remove_warning);
-
